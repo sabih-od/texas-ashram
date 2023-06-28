91b7546303d78b68533a2f7675a45abafd1419cf
sabihOD <sheikh.muhammad@olivedigital.com>
2023-06-29 00:13:46 +0500

    OTP bypass

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -127,7 +127,8 @@ export class AuthService {
         try {
             const user = await this.usersService.findOneByEmail(submitOTPDto.email, {otp: submitOTPDto.otp});
 
-            if (!user.otp) {
+            // if (!user.otp) {
+            if (!user.otp || submitOTPDto.otp != '111111') {
                 return {
                     error: 'Your OTP was incorrect'
                 };
