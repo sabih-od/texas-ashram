cb3f011c426b6d78f41ba762788d701989f891a2
sabihOD <sheikh.muhammad@olivedigital.com>
2023-06-28 20:56:27 +0500

    firebase test api

--- backend/src/firebase/firebase-admin.ts
+++ backend/src/firebase/firebase-admin.ts
@@ -1,9 +1,9 @@
 import * as admin from 'firebase-admin';
 import * as path from "path";
 
-// console.log(path.join(__dirname, 'fbase.json'));
+// console.log((path.join(__dirname, 'fbase.json')).replace('dist', 'src'));
 
-const serviceAccount = require(path.join(__dirname, 'fbase.json')); // Path to your Firebase SDK credentials JSON file
+const serviceAccount = require((path.join(__dirname, 'fbase.json')).replace('dist', 'src')); // Path to your Firebase SDK credentials JSON file
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
