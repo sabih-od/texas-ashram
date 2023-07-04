2bdb9bc7b7d539c15457ccd79414c1229b7a5946
Muhammad Saqib <muhammad.saqib@olivedigital.com>
2023-07-04 19:18:13 +0500

    fixes.

--- admin/next.config.js
+++ admin/next.config.js
@@ -3,21 +3,18 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  },
+  // experimental: {
+  //   esmExternals: false,
+  //   jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+  // },
   publicRuntimeConfig: {
     apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
   },
-  webpack: config => {
-    // config.infrastructureLogging = { debug: /PackFileCache/ }
-    //
-    // config.resolve.alias = {
-    //   ...config.resolve.alias,
-    //   apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    // }
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: "error",
+    };
 
-    return config
-  }
+    return config;
+  },
 }
