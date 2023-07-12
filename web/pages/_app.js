fff5ad4447cf5c7cd9727373ac631380ae7be56f
sabih-od <98338098+sabih-od@users.noreply.github.com>
2023-07-13 00:50:48 +0500

    Web errors (#24)

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,18 +1,26 @@
 import '../public/styles/css/all.min.css';
-import '../public/styles/css/custom.min.css';
+// import '../public/styles/css/custom.min.css';
 import '../public/styles/css/responsive.css';
-import '../public/styles/css/new-html/custom.min.css';
-import React, {useEffect} from "react";
+// import '../public/styles/css/new-html/custom.min.css';
+import React, {useEffect, useState} from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Modal from 'react-modal';
 import Script from 'next/script';
 import Authentication from "../middleware/authentication";
+import {useRouter} from "next/router";
+import {initNav} from '../utils/nav-script'
 
 // Request Prayer Work For Error Handling
 Modal.setAppElement('#__next');
 
 function MyApp({Component, pageProps}) {
+
+    const router = useRouter()
+
+    const [routePath, setRoutePath] = useState(null)
+    const [isJquery, setIsJquery] = useState(false)
+    const [isScrollTrigger, setIsScrollTrigger] = useState(false)
 
     // Animation Timing work
     useEffect(() => {
@@ -22,21 +30,41 @@ function MyApp({Component, pageProps}) {
         });
     }, []);
 
+    useEffect(() => {
+        if (routePath !== router.pathname && isJquery && isScrollTrigger) {
+            setRoutePath(router.pathname)
+            initNav()
+        }
+    }, [router, routePath, isJquery, isScrollTrigger])
+
     return (
         <>
 
             {/*<DynamicJSFiles/>*/}
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"/>
-            <script src="https://unpkg.com/swiper/swiper-bundle.min.js"/>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"/>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"/>
-            {/*<Script src="/styles/js/all.min.js" />*/}
-            {/*<Script src="/styles/js/aos.js" />*/}
-            <Script src="/styles/js/gsap.js" />
-            {/*<Script src="/styles/js/scrollTrigger.js" />*/}
-            <Script type="module" src="/styles/js/custom.min.js" />
+            <Script
+                src="https://code.jquery.com/jquery-3.6.0.min.js"
+                onLoad={() => {
+                    setIsJquery(true)
+                }}
+            />
+            {isJquery ? (
+                <>
+                    <Script src="https://unpkg.com/swiper/swiper-bundle.min.js"/>
+                    <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"/>
+                    <Script
+                        src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"
+                        onLoad={() => {
+                            setIsScrollTrigger(true)
+                        }}
+                    />
+                    {/*<Script src="/styles/js/all.min.js" />*/}
+                    {/*<Script src="/styles/js/aos.js" />*/}
+                    <Script src="/styles/js/gsap.js"/>
+                    {/*<Script src="/styles/js/scrollTrigger.js" />*/}
+                </>
+            ) : null}
             {/*<Authentication>*/}
-                <Component {...pageProps} />
+            <Component {...pageProps} />
             {/*</Authentication>*/}
         </>
     );
