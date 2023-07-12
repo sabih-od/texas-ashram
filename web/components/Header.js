b494ee5de6061a9fcda4ce8a2e6e9b7b7dec9e1d
sabih-od <98338098+sabih-od@users.noreply.github.com>
2023-07-13 00:50:48 +0500

    Web errors (#24)

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import Logo from "../images/logo.png";
 
 const Header = () => {
     return (
         <React.Fragment>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <Head>
-                <link rel="icon" href={Logo} />
-                <link rel="stylesheet" href="/styles/css/all.min.css" />
-                <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-                <link rel="stylesheet" href="/styles/css/custom.min.css" />
-                <link rel="stylesheet" href="/styles/css/new-html/custom.min.css" />
-                <link rel="stylesheet" href="/styles/css/responsive.css" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
                 <title>Texas Christian Ashram</title>
             </Head>
         </React.Fragment>
--- web/components/Layout.js
+++ web/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "./Header";
-import Menu from "../pages/Menu";
+import Menu from "./Menu";
 import Footer from "./Footer";
 
 function Layout({children}) {
